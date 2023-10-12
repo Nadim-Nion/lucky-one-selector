c89adc9eefa6eb43a48885b95b6e0a0f61689d75
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-10-12 21:22:57 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Buy Toys for your child</h1>
+            <h2>Choose 4 Toys</h2>
+        </div>
+    );
+};
+
+export default Header;
